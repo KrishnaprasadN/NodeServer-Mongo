65926ee851de7889ca1c0ffd4e4d28c70a481813
KrishnaprasadN <krishna.prasad@robosoftin.com>
2020-07-17 16:02:48 +0530

    Mongo DB operations now uses Promises instead of Callback Hell. This makes code look neat and a follow api call chaining...

--- index.js
+++ index.js
@@ -5,35 +5,32 @@ const dbOps = require('./operations')
 const url = 'mongodb://localhost:27017'
 const dbname = 'conFusion'
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url)
+    .then((client) => {
+        console.log('Connected to Mongo DB server')
 
-    assert.equal(err, null)
-
-    console.log('Connected to Mongo DB server')
-
-    const db = client.db(dbname)
-
-    dbOps.insertDocument(db, {"name": "DB ops insert", "desc": "testing db ops"}, 'dishes', (result) => {
-        console.log('Insert Document:\n', result.ops)
-
-        dbOps.findDocuments(db, 'dishes', (docs) => {
-            console.log('Found docs:\n', docs)
-
-            dbOps.updateDocument(db, {"name": "DB ops insert"}, 
-                {"desc": "Updated document des"}, 
-                'dishes', 
-                (result) => {
-                console.log('Updated document:\n')
-
-                dbOps.findDocuments(db, 'dishes', (docs) => {
-                    console.log('Found updated docs:\n', docs)
-
-                    db.dropCollection('dished', (result) => {
-                        console.log(result)
-                        client.close()
-                    })
-                })
+        const db = client.db(dbname)
+        dbOps.insertDocument(db, { "name": "DB ops insert", "desc": "testing db ops" }, 'dishes')
+            .then((result) => {
+                console.log('Insert Document:\n', result.ops)
+                return dbOps.findDocuments(db, 'dishes')
             })
-        })
+            .then((docs) => {
+                console.log('Found docs:\n', docs)
+                return dbOps.updateDocument(db, { "name": "DB ops insert" }, { "desc": "Updated document des" }, 'dishes')
+            })
+            .then((result) => {
+                console.log('Updated document:\n')
+                return dbOps.findDocuments(db, 'dishes')
+            })
+            .then((docs) => {
+                console.log('Found updated docs:\n', docs)
+                return db.dropCollection('dishes')
+            })
+            .then((result) => {
+                console.log(result)
+                client.close()
+            })
+            .catch((err) => console.log("Error - ", err))
     })
-})
+    .catch((err) => console.log("Error - ", err))
--- operations.js
+++ operations.js
@@ -1,37 +1,21 @@
 const assert = require('assert')
 
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection)
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null)
-
-        console.log("Document is inserted")
-        callback(result)
-    })
+    return coll.insert(document)
 }
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection)
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null)
-        callback(docs)
-    })
+    return coll.find({}).toArray()
 }
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection)
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null)
-        console.log("Removed the Document - ", document)
-        callback(result)
-    })
+    return coll.deleteOne(document)
 }
 
-exports.updateDocument = (db, document, update, collection, callback) => {
-    const coll = db.collection(collection)
-    coll.updateOne(document, {$set: update}, null, (err, result) => {
-        assert.equal(err, null)
-        console.log("Updated the Document with - ", update)
-        callback(result)
-    })
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
 }
